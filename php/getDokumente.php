09585683c79d46dd817b318fd410f9502b3bae6e
Gipscomm <g-analysis@energie-gipscomm.de>
2020-05-27 15:38:33 +0200

    Repo prep 2

--- php/getDokumente.php
+++ php/getDokumente.php
@@ -11,22 +11,21 @@ $verwaltung = $_POST['verwaltung'];
 $id = $_POST['id'];
 
 if($verwaltung == "Anl"){
-    $query = "SELECT dok_ID, kategorieDok, nameDok, erweiterungDok  FROM dokumente WHERE anl_ID = '$id' ";
+    $query = "SELECT dok_ID, kategorieDok, nameDok, erweiterungDok FROM dokumente WHERE anl_ID = '$id' ";
     $query .= "AND deleted = 'false' ";
 }
 
  if($verwaltung == "Msm"){
-     $query = "SELECT dok_ID, kategorieDok, nameDok, erweiterungDok  FROM dokumente WHERE msm_ID = '$id' ";
+     $query = "SELECT dok_ID, kategorieDok, nameDok, erweiterungDok FROM dokumente WHERE msm_ID = '$id' ";
      $query .= "AND deleted = 'false' ";
 }
 if($verwaltung == "ERng"){
-    $query = "SELECT dok_ID, kategorieDok, nameDok, erweiterungDok  FROM dokumente WHERE eRng_ID = '$id' ";
+    $query = "SELECT dok_ID, kategorieDok, nameDok, erweiterungDok FROM dokumente WHERE eRng_ID = '$id' ";
     $query .= "AND deleted = 'false' ";
 }
 
-$query = "SELECT dok_ID, kategorieDok, nameDok, erweiterungDok  FROM dokumente WHERE anl_ID = '$id' ";
-    $query .= "AND deleted = 'false' ";
-
 $records = queryDB($conn, $query, "read");
-echo $records;
+
+echo json_encode($records);     // CHANGE: The DB data has to be echoed json encoded(before echo $records;) 27.05.2020
+
 ?>
