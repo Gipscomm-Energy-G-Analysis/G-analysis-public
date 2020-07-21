e25334c25dbb70b4bd8658baf4bf9ef2fe6ded6e
Sinan <sdm@energie-gipscomm.de>
2020-07-21 14:47:14 +0200

    Update monitoring.php
    output works but is still badly formatted

--- php/monitoring.php
+++ php/monitoring.php
@@ -6,10 +6,28 @@ require 'DbOperations.php';
 
 $nameDB = "master" ;
 
-$conn = connectToDB( $nameDB ) ;
+$conn1 = connectToDB( $nameDB ) ;
 
-$query = "SELECT * FROM kundenDBs"
+$queryDBs = "SELECT * FROM kundenDBs " ;
+$queryDBs .= "WHERE active = 'true' " ;
 
+$recordsDB = queryDB($conn1, $queryDBs, "read") ;
 
+function lastData($db) {
+    $conn2 = connectToDB( "master" ) ;
+
+    $queryDate = "SELECT TOP(1) time_de FROM [".$db['name']."].[dbo].[data_value_15m] " ;
+    $queryDate .= "ORDER BY time_de DESC " ;
+
+    $recordsTimes = queryDB($conn2, $queryDate, "read") ;
+
+    return $recordsTimes ;
+}
+
+function outputState($dbs) {
+    print_r (array_map('lastData', $dbs)) ;
+}
+
+outputState($recordsDB) ;
 
 ?>
