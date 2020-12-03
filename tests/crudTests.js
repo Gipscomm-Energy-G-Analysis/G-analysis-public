51e443d6bace475f3a165a91ca6bbf3219511ba1
Sinan <sdm@energie-gipscomm.de>
2020-12-03 14:09:19 +0100

    Update crudTests.js

--- tests/crudTests.js
+++ tests/crudTests.js
@@ -1,11 +1,64 @@
+// CRUD -- CREATE READ UPDATE DELETE
+//
+const randomInt =
+    min =>
+    max =>
+    ((Math.random() * (max - min + 1)) << 0) + min
 
-function generateString(length) {
-    const characters ='_- ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = ' ';
-    const charactersLength = characters.length;
-    for ( let i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+const randomFloat =
+    min =>
+    max =>
+    round((Math.random() * (max - min + 1)) + min)
+
+const randomChar =
+    chars =>
+    chars.charAt(Math.floor(Math.random() * chars.length))
+
+const randomString =
+    maxLen =>
+    array(randomInt(1)(maxLen))("")()
+    .map(() => randomChar(" _-ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 "))
+    .join("")
+
+const resetFields =
+    clearFields
+
+const fillForm =
+    elements =>
+    elements.forEach(a => $(head(a)).val(last(a)))
+
+const createRecord =
+    instanz =>
+    $(instanz).trigger("click")
+
+
+// Test Erweiterungen Anlagen
+const testEAnl =
+    () => {
+        const elementeEAnl =
+            [ [ "#nameEAnl", randomString(50) ]
+            , [ "#kuerzelEAnl", randomString(50) ]
+            , [ "#beschreibungEAnl", randomString(500)]
+            ]
+
+        const addOption =
+            () => {
+                $("#optionEAnl").val(randomString(50))
+                $("#btnOptionHinzEAnl").trigger("click")
+            }
+
+        const fillOptionTable =
+            numberOptions =>
+            array(numberOptions)("")()
+            .forEach(addOption)
+
+
+        resetFields("eAnlHinz")
+        fillForm(elementeEAnl)
+        fillOptionTable(randomInt(1)(50))
+        createRecord("#eAnlSpeichern")
+
+
+
+        // const optionen =
     }
-
-    return result.trim();
-}
