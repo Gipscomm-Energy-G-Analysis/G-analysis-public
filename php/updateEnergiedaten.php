e6e8713b6535627f165307b28a140e56a392a586
Sinan <g-analysis@energie-gipscomm.de>
2021-01-14 11:23:05 +0100

    34-Refactored lastDate fn 3

--- php/updateEnergiedaten.php
+++ php/updateEnergiedaten.php
@@ -9,13 +9,22 @@ include('top-cache.php') ;
 require 'helpers.php' ;
 require 'DbOperations.php' ;
 
-function prepareEnergiedatenArguments($db) {
+function prepareEnergiedatenArguments() {
 
     function lastDate($db, $tbl) {
         $query = "SELECT TOP(1) Time FROM ".$tbl." " ;
-        $query += "ORDER BY Time DESC " ;
-        $retVal = queryDB(connectToDB($db), $query, "read")
-        return count($retVal) === 0 ? false : $retVal["Time"] ;
+        $query .= "ORDER BY Time DESC " ;
+        $result = queryDB(connectToDB($db), $query, "read") ;
+        $retVal = "" ;
+
+        if (count($result) === 0) {
+           $retVal = $retVal ;
+        } elseif (gettype($result[0]["Time"]) === "string") {
+           $retVal = $result[0]["Time"] ;
+        } else {
+           $retVal = dateTimeToString($result[0]["Time"]) ;
+        }
+        return $retVal ;
     }
 
     function lastDateCalcMst($db) {
@@ -39,4 +48,8 @@ function prepareEnergiedatenArguments($db) {
 
     $timeIntervals = array_map('extractTimeInterval', $dbs) ;
 
+    return $timeIntervals ;
+
 }
+
+print_r(prepareEnergiedatenArguments()) ;
