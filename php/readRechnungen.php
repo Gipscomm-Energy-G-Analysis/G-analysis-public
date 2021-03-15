14c03cf72555338032b04b328f08fec47e8c89c9
Sinan <g-analysis@energie-gipscomm.de>
2021-03-15 09:44:00 +0100

    Update readRechnungen.php

--- php/readRechnungen.php
+++ php/readRechnungen.php
@@ -17,6 +17,6 @@ $query .= "AND deleted <> 'true' " ;
 
 $records = queryDB( $conn, $query, "read" ) ;
 
-echo json_encode($records, JSON_INVALID_UTF8_IGNORE);
-include('bottom-cache.php');
+echo json_encode($records, JSON_INVALID_UTF8_IGNORE) ;
+include('bottom-cache.php') ;
 ?>
