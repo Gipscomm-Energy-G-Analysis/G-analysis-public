63ba7fd6920f35b221136a053aaa9007200670a2
Sinan <g-analysis@energie-gipscomm.de>
2021-06-01 11:19:15 +0200

    246-Adjusted readSchichtdaten

--- php/Schichtdaten/readSchichtdaten.php
+++ php/Schichtdaten/readSchichtdaten.php
@@ -18,9 +18,21 @@ $query2 .= "WHERE deleted = 0 " ;
 
 $schichten = queryDB( $conn, $query2, "read" ) ;
 
+$query3  = "SELECT schtMdl_ID, lieg_ID, modellBez, anzahl, LEFT(gueltigVon, 10) AS gueltigVon, LEFT(gueltigBis, 10) AS gueltigBis, bisEndeOffen, notiz FROM schichtModelleHist " ;
+$query3 .= "WHERE deleted = 0 " ;
+
+$schichtModelleHist = queryDB( $conn, $query3, "read" ) ;
+
+$query4  = "SELECT schtDat_ID, schtMdl_ID, datum, nr, bezeichnung, LEFT(uhrzeitVon, 5) AS uhrzeitVon, LEFT(uhrzeitBis, 5) AS uhrzeitBis, tagVon, tagBis FROM schichtenHist " ;
+$query4 .= "WHERE deleted = 0 " ;
+
+$schichtenHist = queryDB( $conn, $query4, "read" ) ;
+
 echo json_encode(
     [ "schichtModelle" => $schichtModelle
     , "schichten" => $schichten
+    , "schichtModelleHist" => $schichtModelleHist
+    , "schichtenHist" => $schichtenHist
     ] , JSON_INVALID_UTF8_IGNORE) ;
 include('bottom-cache.php') ;
 ?>
