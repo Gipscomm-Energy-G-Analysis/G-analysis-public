65f356af14d83ac675fddb157635b9ca06df2d09
Sinan <g-analysis@energie-gipscomm.de>
2021-06-21 11:01:02 +0200

    Added gipscAdm query php file

---
+++ php/Administration/confirmPassword.php
@@ -0,0 +1,14 @@
+<?php
+include('top-cache.php') ;
+ini_set ('display_errors', 'On') ;
+
+require '../DbOperations.php' ;
+
+$conn = connectToDB("gipscomm") ;
+$query = "SELECT  * FROM gipscPw" ;
+$records = queryDB($conn, $query, "read") ;
+
+echo json_encode($records, JSON_INVALID_UTF8_IGNORE) ;
+include('bottom-cache.php') ;
+
+?>
