ffab8e008b4141031b042602bbacb74675e74ab2
Sinan <g-analysis@energie-gipscomm.de>
2021-06-22 09:16:51 +0200

    286-Changed folder name from Administration to Rechteverwaltung

---
+++ src/js/Rechteverwaltung/rechteverwaltung.js
@@ -0,0 +1,15 @@
+// Depends on fpCore.js
+// Depends on https://unpkg.com/dexie@latest/dist/dexie.js
+// idxDB is initialized in main.html
+
+"use strict"
+
+const scpRechteverwaltung =
+    freeze (
+        new function () {
+
+            // this.populateIndexedDB =
+            //     position =>
+                
+        }
+    )
