82b8b123b51593966f3cde91a3c62a0117855b9c
Shrey Dhall <DhallShrey@SEASIAINFOTECH.COM>
2021-09-27 13:51:01 +0530

    252-dashboard_tile_size In Progress

--- dashboard/navbar.php
+++ dashboard/navbar.php
@@ -6,8 +6,79 @@
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end act_background">
       
-        <ul class="navbar-nav mr-lg-2">
-        
+        <ul class="navbar-nav mr-lg-2 menu_dashboard_ul">
+          <li class="text-muted menu_dashboard_database font-weight-bold">
+            <select>
+              <option>Agrodur Bad Berleburg</option>
+              <option>Agrodur Kunststofftechnik</option>
+              <option>Alfried Krupp KH</option>
+              <option>AST</option><option>Benjamins</option>
+              <option>ELB</option><option>EST-Edelstahl</option>
+              <option>Familie Derichsweiler</option>
+              <option>Gipscomm Holding</option>
+              <option>HBS-gipshold</option>
+              <option>HBS-Herholz-Tueren</option>
+              <option>Henkedruck</option>
+              <option>Heute + Comp</option>
+              <option>hpg plastics gmbh</option>
+              <option>Huendgen Swisttal</option>
+              <option>InduRade</option>
+              <option>K.H.Schumacher</option>
+              <option>Linsen Druckcenter GmbH</option>
+              <option>MB-DAH-Energy</option>
+              <option>Mustermandant</option>
+              <option>Raderplast</option>
+              <option>Spies</option>
+            </select>
+          </li>
+          <li><a href="#" class="text-muted menu_dashboard font-weight-bold">Dashboard</a></li>
+          
+          <!-- <li class="dropdown">
+            <a href="#" class="text-muted menu_dashboard font-weight-bold dropbtn">Auswertungen</a>
+            <div class="dropdown-content">
+              <a href="#">Link 1</a>
+              <a href="#">Link 2</a>
+              <a href="#">Link 3</a>
+            </div>
+          </li> -->
+          <li>
+            <div class="dropdown">
+              <button class="dropbtn text-muted menu_dashboard font-weight-bold">Auswertungen 
+                <!-- <i class="fa fa-caret-down"></i> -->
+              </button>
+              <div class="dropdown-content">
+                <!-- <div class="header">
+                  <h2>Mega Menu</h2>
+                </div>    -->
+                <div class="row">
+                  <div class="column">
+                    <h3>Berichte</h3>
+                    <a href="#">ISO</a>
+                    <a href="#">Berichte</a>
+                  </div>
+                  <div class="column">
+                    <h3>Diagramme</h3>
+                    <a href="#">Neu erstellen</a>
+                    <a href="#">Gespeichertes Laden</a>
+                  </div>
+                  <div class="column">
+                    <h3>Verbrauchsdatenexport</h3>
+                  </div>
+                  <div class="column">
+                    <h3>Production</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+
+
+
+          <li><a href="#" class="text-muted menu_dashboard font-weight-bold">Stammdaten</a></li>
+          <li><a href="#" class="text-muted menu_dashboard font-weight-bold">Manuell</a></li>
+          <li><a href="#" class="text-muted menu_dashboard font-weight-bold">Optionen</a></li>
+          <li><a href="#" class="text-muted menu_dashboard font-weight-bold">Hilfe</a></li>
+          <li><a href="#" class="text-muted menu_dashboard font-weight-bold"><img src="images/logout2.png"></a></li>
         </ul>
         <ul class="navbar-nav navbar-nav-right">
          
