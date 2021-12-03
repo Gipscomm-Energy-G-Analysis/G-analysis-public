bbc5416627c4465fbdc324dc4b727518090aca7d
Sinan <g-analysis@energie-gipscomm.de>
2021-12-03 14:12:45 +0100

    Update e2e.test.pup.js

--- tests/e2e.test.pup.js
+++ tests/e2e.test.pup.js
@@ -10,6 +10,7 @@ const puppeteer = require('puppeteer');
 
   const browser = await puppeteer.launch({headless: true})
   const page = await browser.newPage()
+  await page.setDefaultNavigationTimeout(0)
   await page.goto('http://localhost:8080', {
     waitUntil: 'networkidle0',
   })
@@ -44,6 +45,84 @@ const puppeteer = require('puppeteer');
   console.log("Test login")
   assert(urlMain)("http://localhost:8080/main.html")("Login")
 
+  // Test navigation to betreuergruppen tab
+  //
+  await page.evaluate(() => document.querySelector("#betrGrpMenu").click(), "#betrGrpMenu")
+
+  const valBgColor = await page.evaluate(() => window.getComputedStyle(document.querySelector('#tabBetrGrp')).getPropertyValue("background-color"))
+  
+  prepRgb = 
+    val =>
+    valBgColor.split(",")
+    .flatMap(a => a.split("("))
+    .flatMap(a => a.split(")"))
+    .filter(a => a !== "")
+    .filter(a => !isNaN(a)).join(",")
+
+  console.log("")
+  console.log("Test betrGrp tab navigation")
+  assert(prepRgb(valBgColor))("206, 214, 222")("Tab navigation")
+
+  // Test creating a new Betreuergruppe
+  //
+  // Test clearing fields
+  //
+  await page.click("#betrGrpHinz")
+
+  await page.waitForSelector("#firmaBetrGrp")
+  const elementFirma = await page.$("#firmaBetrGrp")
+  const valueFirma = await page.evaluate(el => el.textContent, elementFirma)
+
+  await page.waitForSelector("#anzahlMitarbeiterBetrGrp")
+  const elementMitarbeiter = await page.$("#anzahlMitarbeiterBetrGrp")
+  const valueMitarbeiter = await page.evaluate(el => el.textContent, elementMitarbeiter)
+
+  await page.waitForSelector("#anschriftBetrGrp")
+  const elementAnschrift = await page.$("#anschriftBetrGrp")
+  const valueAnschrift = await page.evaluate(el => el.textContent, elementAnschrift)
+
+  await page.waitForSelector("#plzBetrGrp")
+  const elementPlz = await page.$("#plzBetrGrp")
+  const valuePlz = await page.evaluate(el => el.textContent, elementPlz)
+
+  await page.waitForSelector("#ortBetrGrp")
+  const elementOrt = await page.$("#ortBetrGrp")
+  const valueOrt = await page.evaluate(el => el.textContent, elementOrt)
+
+  await page.waitForSelector("#geschaeftsfuehrerBetrGrp")
+  const elementGeschaeftsfuehrer = await page.$("#geschaeftsfuehrerBetrGrp")
+  const valueGeschaeftsfuehrer = await page.evaluate(el => el.textContent, elementGeschaeftsfuehrer)
+
+  await page.waitForSelector("#telefonBetrGrp")
+  const elementTelefon = await page.$("#telefonBetrGrp")
+  const valueTelefon = await page.evaluate(el => el.textContent, elementTelefon)
+
+  await page.waitForSelector("#emailBetrGrp")
+  const elementEmail = await page.$("#emailBetrGrp")
+  const valueEmail = await page.evaluate(el => el.textContent, elementEmail)
+
+  await page.waitForSelector("#notizBetrGrp")
+  const elementNotiz = await page.$("#notizBetrGrp")
+  const valueNotiz = await page.evaluate(el => el.textContent, elementNotiz)
+
+  const allEmpty =
+    () =>
+    [ valueFirma
+    , valueMitarbeiter
+    , valueAnschrift
+    , valuePlz
+    , valueOrt
+    , valueGeschaeftsfuehrer
+    , valueTelefon
+    , valueEmail
+    , valueNotiz
+    ].every(a => a === "")
+
+  console.log("")
+  console.log("Test creating a new Betreuergruppe")
+  console.log("")
+  console.log("## Test clearing fields")
+  assert(allEmpty())(true)("Clear fields")
 
   await browser.close()
 })()
