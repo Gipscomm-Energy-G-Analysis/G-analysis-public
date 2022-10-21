da88ccb0cc36127d0f3662d0f50ec949857d7334
Sinan-David Müller <SDM@energie-gipscomm.de>
2022-10-21 15:11:00 +0200

    big progress -> login works again !

--- php/DbOperations.php
+++ php/DbOperations.php
@@ -21,48 +21,47 @@ class DBConnection
     const DB_DATABASE                   = 'gipscomm';
     const DB_USERNAME                   = 'gipscomm';
     const DB_PASSWORD                   = 'yXFdFioIzNoGFwaQeGHo';
-    const DB_PORT                       = '3306';
 
     public $conn;
 
-    public function __construct(string $db_name)
+    public function __construct(string $nameDB)
     {
         try {
-            $this->conn = $this->dbConnection(self::DB_HOST, $db_name, self::DB_USERNAME, self::DB_PASSWORD, self::DB_PORT);
+            $this->conn = $this->dbConnection(self::DB_HOST, $nameDB, self::DB_USERNAME, self::DB_PASSWORD);
         } catch (PDOException $e) {
             echo 'Connection failed: ' . $e;
         }
     }
 
-    private function dbConnection(string $host, string $dbName, string $userName, string $userPass, string $port)
+    private function dbConnection(string $host, string $nameDB, string $username, string $password): PDO
     {
-        $charset = 'utf8mb4';
-        $opts = [
+        $options = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => true,
         ];
 
-        $dbh = new PDO('sqlsrv:Server=' . $host . ';Database=' . $dbName . ';Port=' . $port . ';', $userName, $userPass, $opts);
+        $dbh = new PDO('sqlsrv:Server=' . $host . ';Database=' . $nameDB . ';', $username, $password, $options);
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         return $dbh;
     }
 }
 
-function connectToDB(string $nameDB): DBConnection
+function connectToDB(string $nameDB): PDO
 {
-    return new DBConnection($nameDB);
+    return (new DBConnection($nameDB))->conn;
 }
 
-function queryDB(DBConnection $conn, string | array $sqlQueries, string $mode): array | string
+function queryDB(PDO $conn, string | array $sqlQueries, string $mode): array
 {
-    $typeQueries = gettype($sqlQueries);
-
-    switch ($typeQueries) {
+    switch (gettype($sqlQueries)) {
         case 'string':
             $prepVal = execQuery($conn, $sqlQueries, $mode);
-            $retVal = $prepVal ? $prepVal : 'queryDB : Invalid query string !! -> ' . $sqlQueries;
+            $retVal =
+                !$prepVal ?
+                ['error' => 'queryDB : Invalid query string !! -> ' . $sqlQueries] :
+                [$prepVal];
             break;
 
         case 'array':
@@ -70,42 +69,50 @@ function queryDB(DBConnection $conn, string | array $sqlQueries, string $mode): 
             foreach ($sqlQueries as $query) {
                 $retArr[] = execQuery($conn, $query, $mode);
             }
-            $retVal = in_array(false, $retArr) ? 'queryDB : Queries are not all executable  !!' : $retArr;
+            $retVal = in_array(false, $retArr) ? ['error' => 'queryDB : Queries are not all executable  !!'] : $retArr;
             break;
 
         default:
-            $retVal = 'The datatype of the passed SQL-query is invalid!'
-                . '\n\nSQL query was passed as ' . $typeQueries
+            $retVal = ['error' => 'The datatype of the passed SQL-query is invalid!'
+                . '\n\nSQL query was passed as ' . gettype($sqlQueries)
                 . '\n\nThe expected datatype is either a string'
-                . '\n(one query) or an array (multiple queries)!';
+                . '\n(one query) or an array (multiple queries)!'];
             break;
     }
 
     return $retVal;
 }
 
-function execQuery(DBConnection $conn_, string $query_, string $mode_): string | array
+function execQuery(PDO $conn_, string $query_, string $mode_): array
 {
     $retVal = '';
 
-    if ($mode_ === 'read') {
-        $result = sqlsrv_query($conn_, $query_);
-        $data = array();
+    try {
+        $stmt = $conn_->query($query_);
+    } catch (Error $err) {
+        echo json_encode(['error' => $err]);
+    }
 
-        while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
-            $data[] = $row;
+    if ($mode_ === 'read') {
+        $result = [];
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            array_push($result, $row);
         }
 
-        $retVal = $result ? $data : 'execQuery : No data could be read !';
+        $stmt->closeCursor();
+
+        unset($stmt);
+
+        $retVal = !$result ? ['error' => 'execQuery : No data could be read !'] : $result;
     } else {
-        $result = sqlsrv_query($conn_, $query_);
-        $retVal = $result ? $query_ : 'execQuery : No data could be inserted or updated !';
+        $retVal = !$stmt ? ['error' => 'execQuery : No data could be inserted or updated !'] : ["query" => $query_];
     }
 
     return $retVal;
 }
 
-function closeDbConn(DBConnection $conn): bool
+function closeDbConn(PDO $conn): void
 {
-    return sqlsrv_close($conn);
+    unset($conn);
 }
--- php/readInstanzen.php
+++ php/readInstanzen.php
@@ -1,280 +1,215 @@
 <?php
 session_start();
-include('top-cache.php');
+
+if (ini_get('register_globals')) {
+    foreach ($_SESSION as $key => $value) {
+        if (isset($GLOBALS[$key]))
+            unset($GLOBALS[$key]);
+    }
+}
+
 error_reporting(-1);
-ini_set ('display_errors', 'On');
+ini_set('display_errors', 'On');
+
 require 'DbOperations.php';
 
-$nameDB = $_POST['nameDB'];
-$_SESSION["nameDB"] = $nameDB;
-$conn = connectToDB($nameDB);
+$_SESSION["nameDB"] = $_POST['nameDB'];
+
+$conn = connectToDB($_SESSION["nameDB"]);
 
 $id = $_POST['id'];
 
-if($id == "adm"){
-  $query = '';
-  if(!empty($_POST['admID'])) {
-    $admID = $_POST['admID'];
-    $query = "SELECT * FROM admins WHERE adm_ID = '$admID' AND deleted_at IS NULL";
+if ($id == "adm") {
+    $query = '';
+    if (!empty($_POST['admID'])) {
+        $admID = $_POST['admID'];
+        $query = "SELECT * FROM admins WHERE adm_ID = " . $admID . "' AND deleted_at IS NULL ";
+    } else {
 
-  } else {
+        $ins = $_POST['ins'];
+        $idIns = $_POST['insID'];
+        $recordSet = $_POST['recordSet'];
 
-    $ins = $_POST['ins'];
-    $idIns = $_POST['insID'];
-    $recordSet = $_POST['recordSet'];
-
-    if(!empty($idIns)) {
-      $query = "SELECT * FROM admins WHERE $ins = '$idIns' AND deleted_at IS NULL";
+        if (!empty($idIns)) {
+            $query = "SELECT * FROM admins WHERE $ins = " . $idIns . " AND deleted_at IS NULL ";
+        }
     }
-  }
+} elseif ($id == "ben") {
+    $query = '';
+    if (!empty($_POST['benID'])) {
+        $benID = $_POST['benID'];
+        $query = "SELECT * FROM benutzer WHERE ben_ID = '" . $benID . "' AND deleted_at IS NULL";
+    } else {
+        $ins = $_POST['ins'];
+        $idIns = $_POST['insID'];
+        $recordSet = $_POST['recordSet'];
 
-}
-elseif($id == "ben"){
-  $query = '';
-  if(!empty($_POST['benID'])) {
-    $benID = $_POST['benID'];
-    $query = "SELECT * FROM benutzer WHERE ben_ID = '$benID' AND deleted_at IS NULL";
-  } else {
-    $ins = $_POST['ins'];
-    $idIns = $_POST['insID'];
-    $recordSet = $_POST['recordSet'];
-
-    if(!empty($idIns)) {
-      $query = "SELECT * FROM benutzer WHERE $ins = '$idIns' OR manGrp_ID = '$idIns' AND deleted_at IS NULL";
+        if (!empty($idIns)) {
+            $query = "SELECT * FROM benutzer WHERE $ins = '" . $idIns . "' OR manGrp_ID = '" . $idIns . "' AND deleted_at IS NULL";
+        }
     }
-  }
-}
-elseif($id == "man"){
-    $manID = $_POST['manID'] ;
+} elseif ($id == "man") {
+    $manID = $_POST['manID'];
 
-    $query = "SELECT * FROM mandanten " ;
-    $query .= "WHERE man_ID = '$manID' " ;
+    $query = "SELECT * FROM mandanten ";
+    $query .= "WHERE man_ID = '" . $manID . "' ";
+} elseif ($id == "org") {
+    $query = "SELECT * FROM organisationen ";
+    $query .= "WHERE deleted = 0 ";
+} elseif ($id == "lieg") {
+    $orgID = $_POST['orgID'];
 
-}
-elseif($id == "org"){
-    $query = "SELECT * FROM organisationen " ;
-    $query .= "WHERE deleted <> 'true' " ;
-}
-elseif($id == "lieg"){
-    $orgID = $_POST['orgID'] ;
+    $query = "SELECT * FROM liegenschaften ";
+    $query .= "WHERE org_ID = " . $orgID . " ";
+    $query .= "AND deleted = 0 ";
+} elseif ($id == "extDl") {
 
-    $query = "SELECT * FROM liegenschaften " ;
-    $query .= "WHERE org_ID = ".$orgID." " ;
-    $query .= "AND deleted <> 'true' " ;
-}
+    $liegID = $_POST['liegID'];
 
+    $query = "SELECT * FROM externeDurchleitungen ";
 
-elseif($id == "extDl"){
+    $query .= "WHERE lieg_ID = " . $liegID . " ";
 
-  $liegID = $_POST['liegID'];
+    $query .= "AND deleted = 0 ";
+} elseif ($id == "ber") {
 
-$query = "SELECT * FROM externeDurchleitungen ";
 
-  $query .= "WHERE lieg_ID = ".$liegID." ";
+    $liegID = $_POST['liegID'];
 
-  $query .= "AND deleted <> 'true' ";
+    $query = "SELECT * FROM bereiche ";
 
-}
 
-elseif($id == "ber"){
+    $query .= "WHERE lieg_ID = " . $liegID . " ";
 
+    $query .= "AND deleted = 0 ";
+} elseif ($id == "mst") {
 
-  $liegID = $_POST['liegID'];
 
-$query = "SELECT * FROM bereiche ";
+    $berID = $_POST['berID'];
 
+    $query = "SELECT * FROM messstellen ";
 
-  $query .= "WHERE lieg_ID = ".$liegID." ";
 
-  $query .= "AND deleted <> 'true' ";
+    $query .= "WHERE ber_ID = '" . $berID . "' ";
 
-}
 
+    $query .= "AND deleted = 0 ";
 
-elseif($id == "mst"){
 
 
-  $berID = $_POST['berID'];
+    $query .= "ORDER BY nameMSt ";
+} elseif ($id == "std") {
 
-$query = "SELECT * FROM messstellen ";
+    $liegID = $_POST['liegID'];
 
+    $query = "SELECT * FROM standorte ";
+    $query .= "WHERE lieg_ID = " . $liegID . " ";
+    $query .= "AND deleted = 0 ";
+} elseif ($id == "stdDr") {
 
-  $query .= "WHERE ber_ID = '$berID' ";
+    $liegID = $_POST['liegID'];
 
+    $query = "SELECT * FROM standorteDritter ";
 
-  $query .= "AND deleted <> 'true' ";
+    $query .= "WHERE lieg_ID = " . $liegID . " ";
 
+    $query .= "AND deleted = 0 ";
+} elseif ($id == "anl" || $id == "anlID") {
 
 
-  $query .= "ORDER BY nameMSt ";
+    $liegID = $_POST['liegID'];
 
-}
+    $query = "SELECT * FROM anlagen ";
 
 
-elseif($id == "std"){
+    if ($id === "anlID") {
 
+        $anlID = $_POST['anlID'];
 
-  $liegID = $_POST['liegID'];
+        $query .= "WHERE anl_ID = " . $anlID . " ";
+    } else {
 
-$query = "SELECT * FROM standorte ";
+        $query .= "WHERE lieg_ID = " . $liegID . " ";
+    }
 
-  $query .= "WHERE lieg_ID = ".$liegID." ";
+    $query .= "AND deleted = 0 ";
+    $query .= "AND archiviertAnl = 0 ";
+} elseif ($id == "msm") {
 
 
+    $liegID = $_POST['liegID'];
 
-  $query .= "AND deleted <> 'true' ";
+    $query = "SELECT * FROM messmittel ";
 
-}
+    $query .= "WHERE lieg_ID = " . $liegID . " ";
 
+    $query .= "AND deleted = 0 ";
+} elseif ($id == "ent") {
 
-elseif($id == "stdDr"){
+    $liegID = $_POST['liegID'];
 
-  $liegID = $_POST['liegID'];
+    $query = "SELECT * FROM energietraeger ";
+    $query .= "WHERE lieg_ID = " . $liegID . " ";
+    $query .= "AND deleted = 0 ";
+} elseif ($id == "vers") {
 
-$query = "SELECT * FROM standorteDritter ";
+    $entID = $_POST['entID'];
 
-  $query .= "WHERE lieg_ID = ".$liegID." ";
+    $query = "SELECT * FROM versorger ";
+    $query .= "WHERE ent_ID = " . $entID . " ";
+} elseif ($id == "enf") {
 
-  $query .= "AND deleted <> 'true' ";
+    $liegID = $_POST['liegID'];
 
-}
+    $query = "SELECT * FROM energieformen ";
+    $query .= "WHERE lieg_ID = " . $liegID . " ";
+} elseif ($id == "ePrd") {
 
-elseif($id == "anl" || $id == "anlID"){
-
-
-	$liegID = $_POST['liegID'];
-
-$query = "SELECT * FROM anlagen ";
-
-
-  if($id === "anlID"){
-
-    $anlID = $_POST['anlID'];
-
-    $query .= "WHERE anl_ID = '$anlID' ";
-
-  }
-
-  else {
-
-    $query .= "WHERE lieg_ID = '$liegID' ";
-
-  }
-
-  $query .= "AND deleted <> 'true' ";
-
-
-  $query .= "AND archiviertAnl <> 'true' ";
-
-}
-
-
-elseif($id == "msm"){
-
-
-	$liegID = $_POST['liegID'];
-
-$query = "SELECT * FROM messmittel ";
-
-  $query .= "WHERE lieg_ID = ".$liegID." ";
-
-  $query .= "AND deleted <> 'true' ";
-
-}
-
-
-elseif($id == "ent"){
-
-    $liegID = $_POST['liegID'] ;
-
-    $query = "SELECT * FROM energietraeger " ;
-    $query .= "WHERE lieg_ID = '$liegID' " ;
-    $query .= "AND deleted = 0 " ;
-
-}
-
-
-elseif($id == "vers"){
-
-    $entID = $_POST['entID'] ;
-
-    $query = "SELECT * FROM versorger " ;
-    $query .= "WHERE ent_ID = '$entID' " ;
-
-}
-
-elseif($id == "enf"){
-
-    $liegID = $_POST['liegID'] ;
-
-    $query = "SELECT * FROM energieformen " ;
-    $query .= "WHERE lieg_ID = '$liegID' " ;
-
-}
-
-elseif($id == "ePrd"){
-
-    $query = "SELECT * FROM erweiterungenProdukte ";
-    $query .= "WHERE deleted <> 'true' ";
-
-}
-
-elseif($id == "eRng"){
+    $query  = "SELECT * FROM erweiterungenProdukte ";
+    $query .= "WHERE deleted = 0 ";
+} elseif ($id == "eRng") {
 
     $liegID = $_POST['liegID'];
 
     $query = "SELECT * FROM externeRechnungen ";
-    $query .= "WHERE lieg_ID = ".$liegID." ";
-    $query .= "AND deleted <> 'true' ";
+    $query .= "WHERE lieg_ID = " . $liegID . " ";
+    $query .= "AND deleted = 0 ";
+} elseif ($id == "intEngIMw") {
 
+    //$liegID = $_POST['liegID'];
+
+    $query = "SELECT * FROM messstellen ";
+
+    $query .= "WHERE messartMst = 'manuell' ";
+
+    $query .= "AND deleted = 0 ";
+} elseif ($id == "intBdeIMw") {
+
+    //$liegID = $_POST['liegID'];
+
+    $query = "SELECT * FROM Anlagen ";
+
+    $query .= "WHERE deleted = 0 ";
+
+    $query .= "AND archiviertAnl = 0 ";
+    /*17-03-2021*/
+    /*new-mm-start*/
+    // $query .= "AND tpy <> 'energiedaten' ";
+    /*new-mm end*/
+} elseif ($id == "zp") {
+
+    $liegID = $_POST['liegID'];
+
+    $query = "SELECT * FROM zaehlpunktnummern ";
+
+    $query .= "WHERE lieg_ID = " . $liegID . " ";
+
+    $query .= "AND deleted = 0 ";
+} elseif ($id == "betrPar") {
+
+    $query = "SELECT * FROM config.betriebsparameter ";
 }
 
-elseif($id == "intEngIMw"){
+echo json_encode(queryDB($conn, $query, "read"));
 
-  //$liegID = $_POST['liegID'];
-
-  $query = "SELECT * FROM messstellen ";
-
-  $query .= "WHERE messartMst = 'manuell' ";
-
-  $query .= "AND deleted <> 'true' ";
-}
-
-elseif($id == "intBdeIMw"){
-
-  //$liegID = $_POST['liegID'];
-
-  $query = "SELECT * FROM Anlagen ";
-
-  $query .= "WHERE deleted <> 'true' ";
-
-  $query .= "AND archiviertAnl <> 'true' ";
-  /*17-03-2021*/
-  /*new-mm-start*/
-  // $query .= "AND tpy <> 'energiedaten' ";
-  /*new-mm end*/
-
-}
-
-elseif($id == "zp"){
-
- $liegID = $_POST['liegID'];
-
- $query = "SELECT * FROM zaehlpunktnummern ";
-
-  $query .= "WHERE lieg_ID = ".$liegID." ";
-
-  $query .= "AND deleted <> 'true' ";
-
-}
-elseif($id == "betrPar"){
-
-  $query = "SELECT * FROM config.betriebsparameter ";
-
-}
-
-$records = queryDB($conn, $query, "read") ;
-echo json_encode($records, JSON_INVALID_UTF8_IGNORE) ;
-
-include('bottom-cache.php');
-?>
+closeDbConn($conn);
